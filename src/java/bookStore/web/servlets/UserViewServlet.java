d4d00d462dc06a72cda45622aac79641b1e1462f
chatterburger97 <paromac@gmail.com>
2018-04-27 13:19:15 +0800

    add google translate element to browse books page

--- src/java/bookStore/web/servlets/UserViewServlet.java
+++ src/java/bookStore/web/servlets/UserViewServlet.java
@@ -6,10 +6,7 @@
 package bookStore.web.servlets;
 
 import bookStore.core.domain.Book;
-import bookStore.core.domain.UserRole;
-import bookStore.core.services.UserService;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,46 +21,11 @@ import java.util.ArrayList;
  */
 public class UserViewServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UserViewServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UserViewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
                 
         HttpSession session = request.getSession();
-        String currentUserName = (String)session.getAttribute("currentUserName");
         String currentUserRole = (String)session.getAttribute("currentUserRole");
         int loyalty = (int)session.getAttribute("currentUserLoyalty");
         
@@ -82,29 +44,5 @@ public class UserViewServlet extends HttpServlet {
             response.sendRedirect("../login");
         }
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
